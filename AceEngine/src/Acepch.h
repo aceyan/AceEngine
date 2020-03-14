4a1f630a8a160b1182ca226c9b9a89199a99dcf1
Kai Yan <kyan2@wpi.edu>
2020-03-14 01:17:21 -0400

    add glfw

--- AceEngine/src/Acepch.h
+++ AceEngine/src/Acepch.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <memory>
+#include <iostream> 
+#include <glfw3.h> 
 
 #ifdef ACE_ENGINE_PLATFORM_WINDOWS
 	#include <Windows.h>
---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# AceEngine
+
+## ThirdParty: 
+Glfw-3.3.2 win64   
+spdlog
